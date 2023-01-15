e394984b2745b3d20fc8728784926e77edb65880
MChassanNajib <chassannajib@gmail.com>
2023-01-15 22:14:12 +0700

    feat [CN] Menambahkan Intro App

---
+++ lib/Page/Screen/SplashScreen/splashscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
